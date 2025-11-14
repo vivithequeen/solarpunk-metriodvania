432b772f8fbfc69e67ff95188d7e22b97a3ae91b
ViviTheQueen <vivithequeen1@gmai.com>
2025-11-14 10:07:44 -0500

    fixed because it broke idk what happened

--- Game/player/movement/Scripts/Movement.cs
+++ Game/player/movement/Scripts/Movement.cs
@@ -1,34 +1,30 @@
 using System;
+using System.Drawing;
 using Godot;
 
 public partial class Movement : CharacterBody2D
 {
-	[Export] public float MaxSpeed = 600f;
-	[Export] public float JumpForce = 90f;
+	[Export] public float MaxSpeed = 700f;
+	[Export] public float JumpForce = 65f;
 	[Export] public float Gravity = 5000f;
 	[Export] public float Acceleration = 80f;
+	
 
 	double timeSinceJump = 0;
 	float speed = 0;
 	bool inJump = false;
 
 	private Vector2 velocity = Vector2.Zero;
+	private AnimatedSprite2D _animPlayer;
+
+	public override void _Ready()
+	{
+		// Get the AnimationPlayer node
+		_animPlayer = GetNode<AnimatedSprite2D>("Player");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		// Apply gravity
-		if (!IsOnFloor())
-		{
-			velocity.Y += Gravity * (float)delta;
-			
-		}
-		else
-		{
-			inJump = false;
-			velocity.Y = 0; // Reset Y velocity when grounded
-			timeSinceJump = 0;
-			
-		}
 		if (IsOnCeiling())
 		{
 			inJump = true;
@@ -39,21 +35,58 @@ public partial class Movement : CharacterBody2D
 		if (Input.IsActionPressed("Right"))
 		{
 			speed = MaxSpeed;
+			_animPlayer.FlipH = false;
+			if (inJump == false)
+			{
+				_animPlayer.Play("Running");
+			}
 		}
 		else if (Input.IsActionPressed("Left"))
 		{
 			speed = -MaxSpeed;
+			_animPlayer.FlipH = true;
+			if (inJump == false)
+			{
+				_animPlayer.Play("Running");
+			}
 		}
 		else
 		{
 			speed = 0;
+			if (inJump == false)
+			{
+				_animPlayer.Play("Idling");
+			}
 		}
+		// Apply gravity
+		if (!IsOnFloor())
+		{
+			velocity.Y += Gravity * (float)delta;
+			_animPlayer.Play("Jumping_down");
+			if (velocity.Y > 0){
+				inJump = true;
+			}
+
+		}
+		else
+		{
+			velocity.Y = 0; // Reset Y velocity when grounded
+			timeSinceJump = 0;
+			inJump = false;
+			
+		}
+		if (Velocity.Y > 0)
+		{
+			_animPlayer.Play("Jumping_down");
+		}
+
 
 		velocity.X = speed;
 
 		// Jumping
 		if (inJump == false && timeSinceJump <= 0.25 && Input.IsActionPressed("Jump"))
 		{
+			_animPlayer.Play("Jumping_up");
 			timeSinceJump += delta;
 			if (timeSinceJump <= 0.03)
 			{
@@ -69,6 +102,11 @@ public partial class Movement : CharacterBody2D
 		{
 			inJump = true;
 			velocity.Y += 600;
+		}
+
+		if (velocity.Y >= 2000)
+		{
+			velocity.Y = 2000;
 		}
 
 
